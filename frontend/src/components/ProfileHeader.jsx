1de697881d65f64f9b82db70e1a1d6c51da16e90
kundan-kumar23 <kundankumar777faridkot@gmail.com>
2026-08-29 17:26:29 +0530

    fix issue of profile pic updation

--- frontend/src/components/ProfileHeader.jsx
+++ frontend/src/components/ProfileHeader.jsx
@@ -37,7 +37,7 @@ function ProfileHeader() {
               onClick={() => fileInputRef.current.click()}
             >
               <img
-                src={selectedImg || authUser.profilePic || "/avatar.png"}
+                src={selectedImg || authUser.profilepic || "/avatar.png"}
                 alt="User image"
                 className="size-full object-cover"
               />
