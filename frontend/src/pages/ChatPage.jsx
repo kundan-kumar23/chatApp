2bf39992830981f5f90a2fdcd88f54f375d36ebf
kundan-kumar23 <kundankumar777faridkot@gmail.com>
2026-08-29 17:45:01 +0530

    responsive to mobile layout

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,3 +1,4 @@
+
 import { useChatStore } from "../store/useChatStore";
 
 import BorderAnimatedContainer from "../components/BorderAnimatedContainer";
@@ -5,33 +6,50 @@ import ProfileHeader from "../components/ProfileHeader";
 import ActiveTabSwitch from "../components/ActiveTabSwitch";
 import ChatsList from "../components/ChatsList";
 import ContactList from "../components/ContactList";
-
 import NoConversationPlaceholder from "../components/NoConversationPlaceholder";
 import Chatcontainer from "../components/Chatcontainer";
-
 
 function ChatPage() {
   const { activeTab, selectedUser } = useChatStore();
 
   return (
-    <div className="relative w-full max-w-6xl h-[800px]">
+    <div className="relative w-full max-w-6xl h-screen md:h-[800px]">
       <BorderAnimatedContainer>
+        
         {/* LEFT SIDE */}
-        <div className="w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col">
+        <div
+          className={`w-full md:w-80 bg-slate-800/50 backdrop-blur-sm flex flex-col
+          ${selectedUser ? "hidden md:flex" : "flex"}`}
+        >
           <ProfileHeader />
+
           <ActiveTabSwitch />
 
           <div className="flex-1 overflow-y-auto p-4 space-y-2">
-            {activeTab === "chats" ? <ChatsList /> : <ContactList />}
+            {activeTab === "chats" ? (
+              <ChatsList />
+            ) : (
+              <ContactList />
+            )}
           </div>
         </div>
 
         {/* RIGHT SIDE */}
-        <div className="flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm">
-          {selectedUser ? <Chatcontainer /> : <NoConversationPlaceholder/>}
+        <div
+          className={`w-full flex-1 flex flex-col bg-slate-900/50 backdrop-blur-sm
+          ${selectedUser ? "flex" : "hidden md:flex"}`}
+        >
+          {selectedUser ? (
+            <Chatcontainer />
+          ) : (
+            <NoConversationPlaceholder />
+          )}
         </div>
+
       </BorderAnimatedContainer>
     </div>
   );
 }
+
 export default ChatPage;
+
